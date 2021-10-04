38d8973519dbbafef073ca6f9b991b70d21f2be3
Nathan Kennedy <nathan.kennedy.1997@gmail.com>
2021-10-05 11:19:52 +1300

    Added error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				message: error.message,
+				status: status
+			}
+		};
+	}
+</script>
+
+<svelte:head>
+	<title>Page Not Found</title>
+</svelte:head>
+
+<p>This page doesn't exist</p>
