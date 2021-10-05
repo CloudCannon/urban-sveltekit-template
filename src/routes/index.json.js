a9240ab1768c13e3e0a3b4aa3519dd6a68c50b50
Nathan Kennedy <nathan.kennedy.1997@gmail.com>
2021-10-06 11:26:38 +1300

    Linting

--- src/routes/index.json.js
+++ src/routes/index.json.js
@@ -1,6 +1,6 @@
 import { getCollectionItem, getCollection } from '$lib/collections.js';
 
-export async function get({ params }) {
+export async function get() {
 	const pageDetails = await getCollectionItem('pages', 'index');
 	const clients = await getCollection('clients');
 
