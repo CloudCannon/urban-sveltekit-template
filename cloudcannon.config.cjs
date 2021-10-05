fda5cb7a185b99f095dedafe896b9df2bfd3b4ba
Nathan Kennedy <nathan.kennedy.1997@gmail.com>
2021-10-06 11:08:55 +1300

    Better cc config

--- cloudcannon.config.cjs
+++ cloudcannon.config.cjs
@@ -1,41 +1,42 @@
 module.exports = {
-    _select_data: {
-      social_icons: ['Facebook', 'Instagram', 'LinkedIn', 'Pinterest', 'Tumblr', 'Twitter', 'Youtube', 'RSS']
-    },
+  _select_data: {
+    social_icons: ['Facebook', 'Instagram', 'LinkedIn', 'Pinterest', 'Tumblr', 'Twitter', 'Youtube', 'RSS']
+  },
 
-    paths: {
-      collections: 'content',
-      data: 'content/data',
-      static: 'public',
-      uploads: 'public/uploads'
-    },
+  paths: {
+    collections: 'content',
+    data: 'content/data',
+    static: 'public',
+    uploads: 'public/uploads'
+  },
 
-    'collections-config': {
-      data: { path: 'content/data' },
-      clients: {
-        path: 'content/clients',
-        url: '/clients/[slug]',
-        'output': true,
-        name: 'Clients'
-      },
-      webpages: {
-        path: 'content/pages',
-        url: '/[slug]',
-        'output': true,
-        name: 'Pages',
-        _icon: 'wysiwyg',
-        _enabled_editors: ['visual','content']
-      },
-      'staff-members': {
-        path: 'content/staff-members',
-        _enabled_editors: ['data'],
-        name: 'Staff Members'
-      },
-      posts: {
-        path: 'content/posts',
-        'output': true,
-        url: '/blog/[slug]',
-        name: 'Blog'
-      }
+  'collections-config': {
+    data: {
+      path: 'content/data',
+      name: 'Data',
+    },
+    clients: {
+      path: 'content/clients',
+      url: '/clients/[slug]',
+      'output': true
+    },
+    pages: {
+      path: 'content/pages',
+      url: '/[slug]',
+      'output': true,
+      _icon: 'wysiwyg',
+      _enabled_editors: ['visual','content']
+    },
+    'staff_members': {
+      path: 'content/staff-members',
+      _enabled_editors: ['data'],
+      name: 'Staff Members'
+    },
+    posts: {
+      path: 'content/posts',
+      'output': true,
+      url: '/blog/[slug]',
+      name: 'Blog'
     }
-  };
+  }
+};
