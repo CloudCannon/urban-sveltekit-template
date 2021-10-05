48d86ceac832ba04a0f03643a835b62a3e647f7f
NJKode <nathan.kennedy.1997@gmail.com>
2021-10-05 19:44:03 +1300

    removed unneeded config

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
-import sveltePreprocess from 'svelte-preprocess';
 
 const config = {
 	extensions: ['.svelte'],
 
-	preprocess: [
-		sveltePreprocess(),
-	],
-
-
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+		adapter: adapter(),
+		target: '#svelte'
 	}
 };
 
